dc15529e08b6fd831aa7d0e5d0d5f87a925daf85
SniiiperZ <cedric1997@hotmail.com>
2024-09-10 15:09:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-red-700">Hello world!</h1>
+</template>
+
+<style scoped></style>
